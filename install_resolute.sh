c45c205545e06c13ac90a96402c343c9bd76309b
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-02-13 07:32:02 -0500

    Add installation script for Resolute

---
+++ install_resolute.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+ 
+set -Eeuxo pipefail
+ 
+: "${OSATE_DIR:=osate2-2.14.0}"
+: "${eclipseRelease:=2023-12}"
+: "${OSATE_VERSION:=2.14.0}"
+: "${OSATE_URL:=https://osate-build.sei.cmu.edu/download/osate/stable/${OSATE_VERSION}/products/osate2-${OSATE_VERSION}-vfinal-linux.gtk.x86_64.tar.gz}"
+: "${RESOLUTE_UPDATE_SITE:=https://raw.githubusercontent.com/loonwerks/Resolute-Updates/master}"
+: "${RESOLUTE_FEATURE_ID:=com.rockwellcollins.atc.resolute.feature.feature.group}"
+ 
+rm -rf ${OSATE_DIR}
+mkdir -p ${OSATE_DIR}
+pushd ${OSATE_DIR}
+curl ${OSATE_URL} | tar --warning=no-unknown-keyword -xz
+popd
+ 
+${OSATE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${RESOLUTE_UPDATE_SITE} -installIU ${RESOLUTE_FEATURE_ID}
